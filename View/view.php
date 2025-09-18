76bccab01e08e35298df42c1b9e1cde67db591be
cjabendan <chrisbend2004@gmail.com>
2025-09-18 21:14:48 +0800

    ah dadih

--- View/view.php
+++ View/view.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . "/Database/database.php";
+require_once __DIR__ . "/../Database/database.php";
 
 if (!isset($_SESSION['formData'])) {
     header("Location: index.php");
